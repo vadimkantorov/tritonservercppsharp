49fc70f3715bd29b3473474f01e3e98d9cfc597b
Vadim Kantorov <vadimkantorov@gmail.com>
2024-08-19 16:21:11 +0200

    ...

--- tritonservercppsharp.cs
+++ tritonservercppsharp.cs
@@ -6,18 +6,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace CppSharpTransformer
-{
-    class DllDemoGenerator : ILibrary
-    {
-        public static void Main(string[] args)
-        {
-            ConsoleDriver.Run(new DllDemoGenerator());
-        }
-
-        void Setup(Driver driver)
-        {
+namespace CppSharpTransformer {
+    class DllDemoGenerator : ILibrary {
+        public static void Main(string[] args) { ConsoleDriver.Run(new DllDemoGenerator()); }
+        public void SetupPasses(Driver driver) { }
+        public void Preprocess(Driver driver, ASTContext ctx) { }
+        public void Postprocess(Driver driver, ASTContext ctx) { }
+        void ILibrary.Setup(Driver driver) { }
+        void Setup(Driver driver) {
             var options = driver.Options;
             options.GeneratorKind = GeneratorKind.CSharp;
             var module = options.AddModule("TritonServer");
@@ -26,10 +22,5 @@ namespace CppSharpTransformer
             //module.LibraryDirs.Add("/path/to/triton/server.so");
             //module.Libraries.Add("tritonserver.so");
         }
-
-        public void SetupPasses(Driver driver) { }
-        public void Preprocess(Driver driver, ASTContext ctx) { }
-        public void Postprocess(Driver driver, ASTContext ctx) { }
-        void ILibrary.Setup(Driver driver) { }
     }
 }
