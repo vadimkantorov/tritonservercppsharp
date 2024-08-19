c1ce823808719283a81dd99414ec06d2db2377d1
Vadim Kantorov <vadimkantorov@gmail.com>
2024-08-19 16:26:09 +0200

    ...

--- tritonservercppsharp.cs
+++ tritonservercppsharp.cs
@@ -1,26 +1,19 @@
 using CppSharp;
 using CppSharp.AST;
 using CppSharp.Generators;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-namespace CppSharpTransformer {
-    class DllDemoGenerator : ILibrary {
-        public static void Main(string[] args) { ConsoleDriver.Run(new DllDemoGenerator()); }
-        public void SetupPasses(Driver driver) { }
-        public void Preprocess(Driver driver, ASTContext ctx) { }
-        public void Postprocess(Driver driver, ASTContext ctx) { }
-        void ILibrary.Setup(Driver driver) { }
-        void Setup(Driver driver) {
-            var options = driver.Options;
-            options.GeneratorKind = GeneratorKind.CSharp;
-            var module = options.AddModule("TritonServer");
-            module.IncludeDirs.Add("core/include");
-            module.Headers.Add("triton/core/tritonserver.h");
-            //module.LibraryDirs.Add("/path/to/triton/server.so");
-            //module.Libraries.Add("tritonserver.so");
-        }
+namespace CppSharpTransformer { class DllDemoGenerator : ILibrary {
+    public static void Main(string[] args) { ConsoleDriver.Run(new DllDemoGenerator()); }
+    public void SetupPasses(Driver driver) { }
+    public void Preprocess(Driver driver, ASTContext ctx) { }
+    public void Postprocess(Driver driver, ASTContext ctx) { }
+    void ILibrary.Setup(Driver driver) { }
+    void Setup(Driver driver) {
+        var options = driver.Options;
+        options.GeneratorKind = GeneratorKind.CSharp;
+        var module = options.AddModule("TritonServer");
+        module.IncludeDirs.Add("core/include");
+        module.Headers.Add("triton/core/tritonserver.h");
+        //module.LibraryDirs.Add("/path/to/triton/server.so");
+        //module.Libraries.Add("tritonserver.so");
     }
-}
+} }
